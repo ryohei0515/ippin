a0b121fc415bf5781af83f546bc566f1448baace
ryohei0515 <besh.515@gmail.com>
2020-12-21 15:38:43 +0900

    Userテストのリファクタリング

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,57 +1,74 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "名前、メールアドレス、パスワードが全て有効" do
+
+  it "name、email、passwordが全て有効" do
     expect{ build(:user).to be_valid }
   end
 
-  it "名前が空白で無効" do
-    expect{ build(:user, name: " ").to_not be_valid }
-    expect{ build(:user, name: nil).to_not be_valid }
+  describe "name:" do
+    it "空白で無効" do
+      expect{ build(:user, name: " ").to_not be_valid }
+      expect{ build(:user, name: nil).to_not be_valid }
+    end
+    it "長すぎるため無効" do
+      expect{ build(:user, name: "a" * 50).to be_valid }
+      expect{ build(:user, name: "a" * 51).to_not be_valid }
+    end
   end
 
-  it "メールアドレスが空白で無効" do
-    expect{ build(:user, email: " ").to_not be_valid }
-    expect{ build(:user, email: nil).to_not be_valid }
+  describe "email:" do
+    it "空白で無効" do
+      expect{ build(:user, email: " ").to_not be_valid }
+      expect{ build(:user, email: nil).to_not be_valid }
+    end
+    it "長すぎるため無効" do
+      expect{ build(:user, email: "a" * 243 + "@example.com").to be_valid }
+      expect{ build(:user, email: "a" * 244 + "@example.com").to_not be_valid }
+    end
+    describe "フォーマットに則っていないため無効" do
+      it "user@example,com" do
+        expect{ build(:user, email: "user@example,com").to_not be_valid }
+      end
+      it "user_at_foo.org" do
+        expect{ build(:user, email: "user_at_foo.org").to_not be_valid }
+      end
+      it "user.name@example." do
+        expect{ build(:user, email: "user.name@example.").to_not be_valid }
+      end
+      it "foo@bar_baz.com" do
+        expect{ build(:user, email: "foo@bar_baz.com").to_not be_valid }
+      end
+      it "foo@bar+baz.com" do
+        expect{ build(:user, email: "foo@bar+baz.com").to_not be_valid }
+      end
+    end
+    it "他ユーザと重複しているため無効" do
+      create(:user)
+      user = build(:user)
+      expect{ user.to_not be_valid }
+      user.email = "other@example.com"
+      expect{ user.to be_valid }
+    end
   end
 
-  it "パスワードが空白で無効" do
-    expect{ build(:user, password: " ").to_not be_valid }
-    expect{ build(:user, password: nil).to_not be_valid }
+  describe "password:" do
+    it "空白で無効" do
+      expect{ build(:user, password: " ", password_confirmation: " ").to_not be_valid }
+      expect{ build(:user, password: nil, password_confirmation: nil).to_not be_valid }
+    end
+    it "短すぎるため無効" do
+      expect{ build(:user, password: "a" * 6, password_confirmation: "a" * 6)
+              .to be_valid }
+      expect{ build(:user, password: "a" * 5, password_confirmation: "a" * 5)
+              .to_not be_valid }
+    end
   end
 
-  it "名前が長すぎるため無効" do
-    expect{ build(:user, name: "a" * 50).to be_valid }
-    expect{ build(:user, name: "a" * 51).to_not be_valid }
-  end
-
-  it "メールアドレスが長すぎるため無効" do
-    expect{ build(:user, email: "a" * 243 + "@example.com").to be_valid }
-    expect{ build(:user, email: "a" * 244 + "@example.com").to_not be_valid }
-  end
-
-  it "パスワードが短すぎるため無効" do
-    expect{ build(:user, password: "a" * 6, password_confirmation: "a" * 6)
-            .to be_valid }
-    expect{ build(:user, password: "a" * 5, password_confirmation: "a" * 5)
-            .to_not be_valid }
-  end
-
-  it "メールアドレスのフォーマットに則っていないため無効" do
-    expect{ build(:user, email: "user@example,com").to_not be_valid }
-    expect{ build(:user, email: "user_at_foo.org").to_not be_valid }
-    expect{ build(:user, email: "user.name@example.").to_not be_valid }
-    expect{ build(:user, email: "foo@bar_baz.com").to_not be_valid }
-    expect{ build(:user, email: "foo@bar+baz.com").to_not be_valid }
-  end
-
-  it "メールアドレスが他ユーザと重複しているため無効" do
-    create(:user)
-    expect{ build(:user).to_not be_valid }
-  end
-
-  it "authenticated?の引数がnilの時、falseを返すこと" do
-    expect{ build(:user).authenticated?(nil).to be_falsey }
+  describe "#authenticated?" do
+    it "引数がnilの時、falseを返すこと" do
+      expect{ build(:user).authenticated?(nil).to be_falsey }
+    end
   end
 
   it "ユーザを削除した時、レビューも併せて削除されること" do
