7bd29c54e98f973f500b48cbf56a08c7b616c4a7
ryohei0515 <besh.515@gmail.com>
2020-12-28 16:58:54 +0900

    ホーム画面のテストを追加

---
+++ spec/system/homes_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "Homes", type: :system, focus: true do
+  let(:user) { FactoryBot.create(:user) }
+  let(:review_list) { FactoryBot.create_list(:review, 30, user: user) }
+
+  it "ホーム画面のレビューが正しく表示されること" do
+    review_list
+    visit root_path
+    aggregate_failures do
+      expect(page).to have_content "#{review_list.count.to_s}件"
+      expect(page).to have_selector '.pagination'
+      for review in review_list[0..4] do
+        expect(page).to have_content review.content
+        expect(page).to have_content review.food
+      end
+    end
+  end
+end
