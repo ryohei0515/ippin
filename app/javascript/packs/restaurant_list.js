80d0eeab573bfcbcc6edcfaf4ca12bac3b184f05
ryohei0515 <besh.515@gmail.com>
2021-02-15 17:50:17 +0900

    [update]レストラン検索画面にturbolinksで遷移したときも正しく表示されるよう修正。

--- app/javascript/packs/restaurant_list.js
+++ app/javascript/packs/restaurant_list.js
@@ -1,7 +1,7 @@
 import Vue from 'vue/dist/vue.esm.js'
 import App from '../restaurant-list.vue'
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('turbolinks:load', () => {
   const app = new Vue({
     el: '#content',
     render: h => h(App)
