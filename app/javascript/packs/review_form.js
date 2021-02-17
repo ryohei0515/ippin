27d6c132eb4a4db44860a894529c26abc9d5ab3d
ryohei0515 <besh.515@gmail.com>
2021-02-17 15:19:02 +0900

    [add]設定値をフォーム画面に反映するよう処理追加。#21

--- app/javascript/packs/review_form.js
+++ app/javascript/packs/review_form.js
@@ -1,20 +1,79 @@
 import Vue from 'vue/dist/vue.esm.js'
 import App from '../select-restaurant.vue'
 
-document.getElementById('select_restaurant_modal_link').addEventListener('click', () => {
-  const app = new Vue({
-    el: '#select-restaurant-modal',
-    render: h => h(App)
-  })
-})
+
 // document.addEventListener('turbolinks:load', () => {
-//   document.getElementById('select_restaurant_modal_link').addEventListener('click', () => {
-//     const searchModal = new Vue({
-//       el: '#select-restaurant-modal',
-//       render: h => h(App)
-//     })
+//   const app = new Vue({
+//     el: '#select-restaurant-modal',
+//     // data() {
+//     //   return {
+//     //     selectRestaurantModalShow: false
+//     //   };
+//     // },
+//     render: h => h(App)
 //   })
 // })
+
+// export default {
+// //   // data() {
+// //   //   return {
+// //   //     results: [],
+// //   //     loadProgress: false,
+// //   //     selectRestaurantModalShow: true
+// //   //   };
+// //   // },
+// //   // methods: {
+// //   //   onLoadStart() {
+// //   //     this.loadProgress = true;
+// //   //   },
+// //   //   onLoadComplete({ results }) {
+// //   //     this.results = results;
+// //   //     this.loadProgress = false;
+// //   //   },
+// //   //   selectRestaurantModalOpen() {
+// //   //     this.selectRestaurantModalShow = true
+// //   //   },
+// //   //   selectRestaurantModalClose() {
+// //   //     this.selectRestaurantModalShow = false
+// //   //   },
+// //   // },
+//   components: {
+//     App,
+//   },
+//   methods: {
+//     testtest() {
+//       alert("testtest")
+//       this.$refs.App.selectRestaurantModalOpen()
+//     }
+//   },
+// };
+
+// export default {
+//   data() {
+//     return {
+//       term: ""
+//     }
+//   },
+//   methods: {
+//     async exe() {
+//       if (!this.term) {
+//         alert('検索条件を入力してください');
+//         return
+//       }
+//       this.term = this.term.replace(/　/g, " ");
+//       this.$emit("loadStart");
+//     },
+//   },
+// };
+
+document.addEventListener('turbolinks:load', () => {
+  // document.getElementById('select_restaurant_modal_link').addEventListener('click', () => {
+    const searchModal = new Vue({
+      el: '#select-restaurant-modal',
+      render: h => h(App)
+    // })
+  })
+})
 // new Vue({
 //   el: '#search-restaurant-modal',
 //   data: {
--- app/javascript/select-restaurant.vue
+++ app/javascript/select-restaurant.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="overlay z-10 fixed top-0 left-0 w-full h-full">
-    <div class="content z-20 bg-white max-h-11/12 w-11/12 rounded">
+<div>
+  <div>
+    <input class="text-center rounded py-2 bg-blue-600 hover:bg-blue-600 text-white w-20 ml-2" type="button" value="new選択する" @click.self="modalOpen">
+    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-blue-200" type="text" name="review[restaurant]" id="review_restaurant" v-model="restaurant_name" readonly>
+  </div>
+  <div class="overlay z-10 fixed top-0 left-0 w-full h-full" @click.self="modalClose" v-show="modalShow" @test="modalOpen">
+    <div class="content z-20 bg-white max-h-11/12 w-11/12 lg:container rounded">
       <Search class="search" @loadStart="onLoadStart" @loadComplete="onLoadComplete"/>
-      <Result class="result overflow-y-scroll" :results="results" :loadProgress="loadProgress" :scrollable="true"/>
+      <Result class="result overflow-y-scroll" :results="results" :loadProgress="loadProgress" @selectRestaurant="selectItem"/>
       <div class="mx-auto w-32 my-2">
         <a class="mx-auto" href="https://webservice.recruit.co.jp/"><img src="https://webservice.recruit.co.jp/banner/hotpepper-s.gif" alt="ホットペッパー Webサービス" width="135" height="17" border="0" title="ホットペッパー Webサービス"></a>
       </div>
     </div>
   </div>
+</div>
 </template>
 
 <script>
@@ -19,6 +25,9 @@ export default {
     return {
       results: [],
       loadProgress: false,
+      modalShow: false,
+      restaurant_name: "",
+      restaurant_id: ""
     };
   },
   methods: {
@@ -28,6 +37,17 @@ export default {
     onLoadComplete({ results }) {
       this.results = results;
       this.loadProgress = false;
+    },
+    modalOpen() {
+      this.modalShow = true;
+    },
+    modalClose() {
+      this.modalShow = false;
+    },
+    selectItem({ restaurant }) {
+      this.restaurant_id = restaurant.id;
+      this.restaurant_name = restaurant.name;
+      this.modalShow = false;
     },
   },
   components: {
