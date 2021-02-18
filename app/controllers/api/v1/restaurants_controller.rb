b3a9dfd40057263354188ebd88453d4b461709e9
ryohei0515 <besh.515@gmail.com>
2021-02-18 14:53:27 +0900

    [add]外部APIアクセス処理を外部処理化

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -3,35 +3,17 @@
 module Api
   module V1
     class RestaurantsController < ApiController
+      include HotpepperApi
       # HOTPEPPERのAPIより、検索条件に該当するレストラン情報を取得する。
       def index
-        render json: { results: { error: { message: 'paramが不正です' } } } and return if params[:term].nil?
-
-        api_params = {
-          key: Rails.application.credentials.api_key[:HOTPEPPER],
-          keyword: params[:term],
-          format: 'json',
-          count: '100'
-        }
-        uri = URI.parse("http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?#{api_params.to_query}")
-        response = Net::HTTP.start(uri.host, uri.port) do |http|
-          http.get(uri.request_uri)
-        end
-        render json: JSON.parse(response.body)
+        r = search_restaurant(params[:term])
+        render json: r
       end
 
       # HOTPEPPERのAPIより、IDに合致するレストラン情報を取得する。
       def show
-        api_params = {
-          key: Rails.application.credentials.api_key[:HOTPEPPER],
-          id: params[:id],
-          format: 'json'
-        }
-        uri = URI.parse("http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?#{api_params.to_query}")
-        response = Net::HTTP.start(uri.host, uri.port) do |http|
-          http.get(uri.request_uri)
-        end
-        render json: JSON.parse(response.body)
+        r = search_restaurant_by_id(params[:id])
+        render json: r
       end
     end
   end
---
+++ app/controllers/concerns/hotpepper_api.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+module HotpepperApi
+  extend ActiveSupport::Concern
+
+  API_KEY = Rails.application.credentials.api_key[:HOTPEPPER]
+  FORMAT = 'json'
+  API_URL = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?'
+
+  # 検索条件に合致するレストランを取得する、半角スペースで複数条件指定可能
+  def search_restaurant(term, count = 100)
+    return json_error_msg('ERROR: 条件指定が不正です') if term.blank?
+
+    api_params = {
+      key: API_KEY,
+      keyword: term,
+      format: FORMAT,
+      count: count
+    }
+    uri = URI.parse(API_URL + api_params.to_query)
+    api_access(uri)
+  end
+
+  # idが合致するレストランを取得する、カンマ区切りで複数選択可能
+  def search_restaurant_by_id(id)
+    return json_error_msg('idが不正です') if id.blank?
+
+    api_params = {
+      key: API_KEY,
+      keyword: id,
+      format: FORMAT
+    }
+    uri = URI.parse(API_URL + api_params.to_query)
+    api_access(uri)
+  end
+end
+
+private
+
+# httpでアクセスし、結果をJSON形式で返却する。
+def api_access(uri)
+  response = Net::HTTP.start(uri.host, uri.port) do |http|
+    http.open_timeout = 5  # 接続時に待つ最大秒数
+    http.get(uri.request_uri)
+  end
+
+  case response
+  when Net::HTTPSuccess
+    JSON.parse(response.body)
+  else
+    json_error_msg("HTTP ERROR: code=#{response.code} message=#{response.message}")
+  end
+end
+
+def json_error_msg(msg)
+  { results: { error: { message: msg } } }
+end
