97b39b62f472f397d1a68263e6408d3a365d8983
ryohei0515 <besh.515@gmail.com>
2021-02-23 17:14:12 +0900

    [add]Foodモデルへバリデーションを追加。#35

--- app/models/food.rb
+++ app/models/food.rb
@@ -5,4 +5,7 @@ class Food < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 30 },
                    uniqueness: { case_sensitive: false }
+  validates :name_kana, length: { maximum: 50 }
+  validates :category, presence: true, length: { maximum: 30 }
+  validates :description, length: { maximum: 200 }
 end
--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -3,5 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to have_many(:shop_foods) }
+
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_length_of(:name).is_at_most(30) }
+  it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+
+  it { is_expected.to validate_length_of(:name_kana).is_at_most(50) }
+
+  it { is_expected.to validate_presence_of(:category) }
+  it { is_expected.to validate_length_of(:category).is_at_most(30) }
+
+  it { is_expected.to validate_length_of(:description).is_at_most(200) }
 end
