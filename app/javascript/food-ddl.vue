7659970fdbd7383ec78567f363a79389f84485aa
ryohei0515 <besh.515@gmail.com>
2021-02-24 14:40:43 +0900

    [add]ドロップダウンリストを選択できるよう、機能追加。#39

--- app/javascript/food-ddl.vue
+++ app/javascript/food-ddl.vue
@@ -1,13 +1,13 @@
 <template>
-<div class="w-80 relative">
+<div class="w-full relative">
   <div lass="relative" @click="openAndCloseDdl">
-    <input type="text" id="display-text-box" readonly class="border w-full">
+    <input type="text" id="display-text-box" :value="selectText" readonly class="border w-full pr-8">
     <i class="fa fa-chevron-down absolute right-2 top-0 bottom-0 m-auto" aria-hidden="true"></i>
   </div>
-  <div v-if="listShow" class="p-2 absolute z-20 border bg-white shadow-lg w-full">
-    <input type="text" ref="filterTextBox" id="filter-text-box" class="border w-full" v-model="filterText">
+  <div v-show="listShow" class="p-2 absolute z-20 border bg-white shadow-lg w-full">
+    <input type="text" ref="filterTextBox" id="filter-text-box" class="border w-full" v-model="filterText" placeholder="複数検索条件可能（例:北京 餃子）">
     <ul class="overflow-y-scroll" style="max-height: 40vh;">
-      <li class="hover:bg-blue-300" v-for="food of filteredItem" :key="food.id">{{ food.category }} | {{ food.name }} | {{ food.name_kana }}</li>
+      <li class="hover:bg-blue-300" v-for="food of filteredItem" :key="food.id" @click="selectItem(food)">{{ displayText(food) }}</li>
     </ul>
   </div>
   <!-- <div class="my-5">{{ foods }}</div> -->
@@ -22,7 +22,8 @@ export default {
     return {
       foods: [],
       listShow: false,
-      filterText: ""
+      filterText: "",
+      selectText: ""
     };
   },
   async created() {
@@ -32,6 +33,16 @@ export default {
     openAndCloseDdl() {
       this.listShow = !this.listShow;
     },
+    selectItem(item) {
+      this.selectText = this.displayText(item);
+      this.listShow = false;
+    },
+    displayText(item) {
+      var txt = item.category + " | " + item.name
+      if (item.name_kana) txt += " | " + item.name_kana
+      return txt
+    },
+
   },
   computed: {
     filteredItem() {
@@ -44,7 +55,7 @@ export default {
       return this.foods.data.filter(function(item){
         return pattern.test(item.category + item.name + item.name_kana);
       })
-    },
+    }
   },
   watch: {
     listShow(val) {
