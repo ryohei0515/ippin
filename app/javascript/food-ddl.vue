e6a10a7f9fbc5a11c42c095a0f46d83dcf0c442e
ryohei0515 <besh.515@gmail.com>
2021-02-24 13:28:13 +0900

    [add]ドロップダウンリストを作成。リストを表示し、フィルター用のテキストボックスでリストを抽出できるような仕様。#39

--- app/javascript/food-ddl.vue
+++ app/javascript/food-ddl.vue
@@ -1,5 +1,13 @@
 <template>
-<div>
+<div class="w-80 relative">
+  <input type="text" id="display-text-box" readonly @click="openAndCloseDdl" class="border w-full">
+  <div v-if="listShow" class="p-2 absolute z-20 border bg-white shadow-lg w-full">
+    <input type="text" ref="filterTextBox" id="filter-text-box" class="border w-full" v-model="filterText">
+    <ul class="overflow-y-scroll" style="max-height: 40vh;">
+      <li class="hover:bg-blue-300" v-for="food of filteredItem" :key="food.id">{{ food.category }} | {{ food.name }} | {{ food.name_kana }}</li>
+    </ul>
+  </div>
+  <!-- <div class="my-5">{{ foods }}</div> -->
 </div>
 </template>
 
@@ -10,11 +18,40 @@ export default {
   data() {
     return {
       foods: [],
+      listShow: false,
+      filterText: ""
     };
   },
   async created() {
     this.foods = await axios.get('/api/v1/foods/');
-  }
+  },
+  methods: {
+    openAndCloseDdl() {
+      this.listShow = !this.listShow;
+    },
+  },
+  computed: {
+    filteredItem() {
+      if (!this.filterText) return this.foods.data
+      var txt = "^(?=.*" + this.filterText + ")"
+      // スペース区切りの検索条件は複数条件と扱う
+      txt = txt.replace(/　/g, " ").replace(/ /g, ")(?=.*");
+      var pattern = new RegExp(txt, "i")
+
+      return this.foods.data.filter(function(item){
+        return pattern.test(item.category + item.name + item.name_kana);
+      })
+    },
+  },
+  watch: {
+    listShow(val) {
+      if(val) {
+        this.$nextTick(() => {
+          this.$refs.filterTextBox.focus();
+        });
+      }
+    }
+  },
 };
 </script>
 
