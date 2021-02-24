a6fb907a01dec26a5586dc125fe838b8202e1fab
ryohei0515 <besh.515@gmail.com>
2021-02-24 17:40:10 +0900

    [add]ドロップダウンリストの範囲外をクリックした際も閉じるよう、機能を追加。#39

--- app/javascript/food-ddl.vue
+++ app/javascript/food-ddl.vue
@@ -1,10 +1,10 @@
 <template>
-<div class="w-full relative">
-  <div lass="relative" @click="openAndCloseDdl">
+<div id="food-ddl" class="w-full relative">
+  <div class="relative" @click="openAndCloseDdl">
     <input type="text" id="display-text-box" :value="selectText" readonly class="border w-full pr-8">
     <i class="fa fa-chevron-down absolute right-2 top-0 bottom-0 m-auto" aria-hidden="true"></i>
   </div>
-  <div v-show="listShow" class="p-2 absolute z-20 border bg-white shadow-lg w-full">
+  <div v-if="listShow" class="p-2 absolute z-20 border bg-white shadow-lg w-full">
     <input type="text" ref="filterTextBox" id="filter-text-box" class="border w-full" v-model="filterText" placeholder="複数条件入力可能です（例:北京　餃子）">
     <ul class="overflow-y-scroll" style="max-height: 40vh;">
       <li class="bg-yellow-100" v-show="filteredItem.length == 0">ヒットする料理がありません。再入力してください。</li>
@@ -29,28 +29,39 @@ export default {
   async created() {
     this.foods = await axios.get('/api/v1/foods/');
   },
+  mounted() {
+    document.addEventListener('click', (e) => {
+      if(!e.target.closest('#food-ddl')) {
+        this.closeDdl();
+      }
+    })
+  },
   methods: {
     openAndCloseDdl() {
       this.listShow = !this.listShow;
     },
+    closeDdl() {
+      this.listShow = false;
+      this.filterText = "";
+    },
     selectItem(item) {
       this.selectText = this.displayText(item);
-      this.listShow = false;
+      this.closeDdl();
     },
     displayText(item) {
-      var txt = item.category + " | " + item.name
-      if (item.name_kana) txt += " | " + item.name_kana
-      return txt
+      var txt = item.category + " | " + item.name;
+      if (item.name_kana) txt += " | " + item.name_kana;
+      return txt;
     },
 
   },
   computed: {
     filteredItem() {
       if (!this.filterText) return this.foods.data
-      var txt = "^(?=.*" + this.filterText + ")"
+      var txt = "^(?=.*" + this.filterText + ")";
       // スペース区切りの検索条件は複数条件と扱う
       txt = txt.replace(/　/g, " ").replace(/ /g, ")(?=.*");
-      var pattern = new RegExp(txt, "i")
+      var pattern = new RegExp(txt, "i");
 
       return this.foods.data.filter(function(item){
         return pattern.test(item.category + item.name + item.name_kana);
