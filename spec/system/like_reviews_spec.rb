632d8d5ba8128181e1473e9a2053f2aa307ce887
ryohei0515 <besh.515@gmail.com>
2021-03-03 11:39:47 +0900

    [fix]Likeボタンの名称を変数化。

--- spec/system/like_reviews_spec.rb
+++ spec/system/like_reviews_spec.rb
@@ -12,49 +12,51 @@ RSpec.describe 'likeReviews', type: :system, js: true do
   let(:own_review) { FactoryBot.create(:review, user: user) }
 
   before do
+    @like_button = '参考になった'
+    @unlike_button = '取消'
     log_in_as user
     visit shop_food_path(review.shop_food_id)
   end
 
   it 'likeしていないreviewに対し、like用のボタンを押すことで、登録されること' do
     expect do
-      click_button '参考になった'
-      wait_for_loaded_until_css_disappeared('input[value="参考になった"]')
+      click_button @like_button
+      wait_for_loaded_until_css_disappeared("input[value='#{@like_button}']")
     end.to change { user.like_reviews.count }.by(1)
-    expect(page).to_not have_button '参考になった'
-    expect(page).to have_button '取消'
+    expect(page).to_not have_button @like_button
+    expect(page).to have_button @unlike_button
   end
 
   it 'like済のreviewに対し、like用のボタンを押すことで、解除されること' do
-    click_button '参考になった'
-    wait_for_loaded_until_css_disappeared('input[value="参考になった"]')
+    click_button @like_button
+    wait_for_loaded_until_css_disappeared("input[value='#{@like_button}']")
 
     expect do
-      click_button '取消'
-      wait_for_loaded_until_css_disappeared('input[value="取消"]')
+      click_button @unlike_button
+      wait_for_loaded_until_css_disappeared("input[value='#{@unlike_button}']")
     end.to change { user.like_reviews.count }.by(-1)
-    expect(page).to_not have_button '取消'
-    expect(page).to have_button '参考になった'
+    expect(page).to_not have_button @unlike_button
+    expect(page).to have_button @like_button
   end
 
   it 'like用のボタンをクリックするたびに、画面のliked_usersの人数が更新されること' do
     expect(page).to have_content "#{review.liked_users.count}人"
 
     FactoryBot.create_list(:like, Random.rand(2..5), review: review)
-    click_button '参考になった'
-    wait_for_loaded_until_css_disappeared('input[value="参考になった"]')
+    click_button @like_button
+    wait_for_loaded_until_css_disappeared("input[value='#{@like_button}']")
     expect(page).to have_content "#{review.liked_users.count}人"
 
     FactoryBot.create_list(:like, Random.rand(2..5), review: review)
-    click_button '取消'
-    wait_for_loaded_until_css_disappeared('input[value="取消"]')
+    click_button @unlike_button
+    wait_for_loaded_until_css_disappeared("input[value='#{@unlike_button}']")
     expect(page).to have_content "#{review.liked_users.count}人"
   end
 
   it 'ユーザ自身のreviewに対し、like用のボタンが表示されないこと' do
     visit shop_food_path(own_review.shop_food_id)
 
-    expect(page).to_not have_button '参考になった'
-    expect(page).to_not have_button '取消'
+    expect(page).to_not have_button @like_button
+    expect(page).to_not have_button @unlike_button
   end
 end
