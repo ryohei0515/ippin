e765af06c205fcc6bcaab0df5672c3f7cd190aae
ryohei0515 <besh.515@gmail.com>
2021-03-07 15:41:24 +0900

    [fix]seedでデータを作成時、画像も併せて登録できるよう修正。

--- db/fixtures/development/010_user.rb
+++ db/fixtures/development/010_user.rb
@@ -7,8 +7,8 @@ CSV.foreach(file_path, encoding: 'UTF-8', headers: :first_row) do |r|
     s.id = r['id']
     s.email = r['email']
     s.name = r['name']
-    s.picture = r['picture']
     s.password = r['password']
     s.password_confirmation = r['password']
+    s.picture = Rails.root.join("db/fixtures/resource/temp_picture/#{r['picture']}").open if r['picture'].present?
   end
 end
--- db/fixtures/production/900_update_temp_data.rb
+++ db/fixtures/production/900_update_temp_data.rb
@@ -1,49 +1,43 @@
 require 'csv'
 
-# ポートフォリオ用のサンプルデータ作成用。
-# User   :ユーザ画像の登録 
-# Review :レビュー画像登録、作成日・更新日の変更。
-# seed_fuでは以下のコードが実行できないため、コンソール上から下記の処理を実行する。
-# $ load "./db/fixtures/development/900_update_temp_data.rb"
-# $ update_temp_data
-def update_temp_data
-  # User更新
-  User.all.each do |user|
-    user.update(picture: open("#{Rails.root}/db/fixtures/resource/temp_picture/#{user.picture}")) if user.picture.present?
-  end
+# ポートフォリオ用のReview用画像データ登録用。
+class UpdateTempData
+  def self.exec
+    use_count = 5 # 1枚の画像につき、何reviewで使用するか設定
 
-  # Review更新
-  file_path = "db/fixtures/#{Rails.env}/csv/review__picture.csv"
-  use_count = 5 # 1枚の画像につき、何reviewで使用するか設定
+    use_pic_limit = {}
+    Food.all.each do |food|
+      use_pic_limit[food.id] = 0
+    end
 
-  use_pic_limit = {}
-  Food.all.each do |food|
-    use_pic_limit[food.id] = 0
-  end
+    pictures = {}
+    review_file_path = "db/fixtures/#{Rails.env}/csv/review__picture.csv"
+    CSV.foreach(review_file_path, encoding: 'UTF-8', headers: :first_row) do |r|
+      food_id = r['food_id'].to_i
+      pictures[food_id] = [] unless pictures.key?(food_id)
+      pictures[food_id] << { picture: "#{Rails.root}/db/fixtures/resource/temp_picture/#{r['picture']}",
+                             picture_cc: r['picture_cc'] }
+      use_pic_limit[food_id] += use_count
+    end
 
-  pictures = {}
-  CSV.foreach(file_path, encoding: 'UTF-8', headers: :first_row) do |r|
-    food_id = r['food_id'].to_i
-    pictures[food_id] = [] unless pictures.key?(food_id)
-    pictures[food_id] << { picture: "#{Rails.root}/db/fixtures/resource/temp_picture/#{r['picture']}",
-                           picture_cc: r['picture_cc'] }
-    use_pic_limit[food_id] += use_count
-  end
-
-  start_date = Date.parse("2020/09/01")
-  end_date = Date.parse("2021/03/01")
-  Review.all.each do |rev|
-    date = Random.rand(start_date .. end_date)
-    food_id = rev.shop_food.food_id
-    # rateが低いときは画像を登録しないように
-    if use_pic_limit[food_id] > 0 && rev.rate >= 4
-      num = use_pic_limit[food_id] % use_count
-      rev.update(created_at: date, updated_at: date,
-                 picture: open(pictures[food_id][num][:picture]),
-                 picture_cc: pictures[food_id][num][:picture_cc])
-      use_pic_limit[food_id] -= 1
-    else
-      rev.update(created_at: date, updated_at: date)
+    start_date = Date.parse("2020/09/01")
+    end_date = Date.parse("2021/03/01")
+    Review.all.each do |rev|
+      date = Random.rand(start_date .. end_date)
+      food_id = rev.shop_food.food_id
+      # rateが高く、画像の使用制限を超えていないときのみ登録する
+      if use_pic_limit[food_id] > 0 && rev.rate >= 4
+        num = use_pic_limit[food_id] % use_count
+        rev.update(created_at: date, updated_at: date,
+                   picture: open(pictures[food_id][num][:picture]),
+                   picture_cc: pictures[food_id][num][:picture_cc])
+        use_pic_limit[food_id] -= 1
+      else
+        rev.update(created_at: date, updated_at: date,
+                   picture: nil, picture_cc: nil)
+      end
     end
   end
 end
+
+UpdateTempData.exec
