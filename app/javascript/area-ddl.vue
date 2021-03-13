fcaae5e80fe6ffd7f31ed7102ff709d8b581de9c
ryohei0515 <besh.515@gmail.com>
2021-03-13 22:25:44 +0900

    [fix]エリアのドロップダウンリストの背景色を白に指定。#105

--- app/javascript/area-ddl.vue
+++ app/javascript/area-ddl.vue
@@ -3,7 +3,7 @@
     <div>
       <div class="input-label">都道府県</div>
       <div class="w-24 relative text-gray-700">
-        <select :name="largeAreaName" v-model="largeAreaSelected" class="pr-8 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+        <select :name="largeAreaName" v-model="largeAreaSelected" class="pr-8 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-white">
           <option :value="initLargeArea" v-if="initLargeArea">{{ initLargeArea }}</option>
           <option value="">-</option>
           <option v-for="areaName in largeAreas" :value="areaName" :key="areaName">
@@ -16,7 +16,7 @@
     <div class="mt-2 sm:ml-6 sm:mt-0">
       <div class="input-label">エリア</div>
       <div class="w-72 relative text-gray-700">
-        <select :name="middleAreaName" :value="middleAreaSelected"  class="pr-8 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+        <select :name="middleAreaName" :value="middleAreaSelected"  class="pr-8 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-white">
           <option :value="initMiddleArea" v-if="initMiddleArea && initLargeArea == largeAreaSelected">{{ initMiddleArea }}</option>
           <option value="">-</option>
           <option v-for="areaName in middleAreas" :value="areaName" :key="areaName">
