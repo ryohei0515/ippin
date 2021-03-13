fb11bd127459a7d66835d4073286a44081f34ff1
ryohei0515 <besh.515@gmail.com>
2021-03-13 12:32:55 +0900

    [update]likeデータ作成処理の確率の指定が誤っていたため、修正。

--- db/fixtures/development/050_like.rb
+++ db/fixtures/development/050_like.rb
@@ -4,7 +4,7 @@ rate = rand(0..35) # likeに登録する割合
 users = User.where("id <= #{sample_user_count} and id != #{Settings.sample_user.id}")
 Review.all.each do |review|
   users.each do |user|
-    next if rand(100) < rate || review.user_id == user.id
+    next if rand(1..100) > rate || review.user_id == user.id
     Like.seed do |s|
       s.user_id = user.id
       s.review_id = review.id
