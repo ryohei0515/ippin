0523a12ad98f3b98c576aa9c9098f33efed76a86
ryohei0515 <besh.515@gmail.com>
2021-03-14 13:08:21 +0900

    [update]ログイン後、完了メッセージを表示するよう修正。

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     if @user&.authenticate(params[:session][:password])
       log_in @user
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      flash[:success] = 'ログインしました'
       redirect_back_or @user
     else
       flash.now[:danger] = 'メールアドレスとパスワードが不正です'
